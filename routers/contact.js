4b663b19ba1a28bce4ad7f74932dd346458695f8
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-02-06 16:24:34 -0500

    created models and routers directorys and files, contact.js to route to front end, and modelContact.js for the schema for contact

---
+++ routers/contact.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Contact = require('../models/modelContact');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    try {
+        // Create a new entry
+        const newContact = new Contact({
+            name,
+            email,
+            message,
+        });
+
+        await newContact.save();
+
+        res.status(200).json({ message: 'Message sent successfully.'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'something went wrong, please try again' });  
+     }    
+});
+
+module.exports = router;
